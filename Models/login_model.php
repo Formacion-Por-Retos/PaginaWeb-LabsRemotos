cf6a40b711a1aa1c8930a24662ca7e775b6240fb
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-04-28 00:34:56 -0500

    Mejoras de diseño.

--- Models/login_model.php
+++ Models/login_model.php
@@ -5,15 +5,19 @@ require 'conexion_db.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$query = "SELECT COUNT(*) as contar,id from Users where email = '$email' and password = '$password'";
+
+
+$query = "SELECT COUNT(*) as contar,Users.id  from Users where email = '$email' and password = '$password'";
 
 $bdconect = mysqli_query($conexion,$query);
 $parametros = mysqli_fetch_array($bdconect);
 
+
+
 $id = $parametros['id'];
 
-if($parametros['contar']>0){
 
+if($parametros['contar']>0){
     session_start();
     $_SESSION['id'] = $id;
 
